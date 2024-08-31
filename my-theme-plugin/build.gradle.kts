b4dec14ea4465d768cdcd3b5d2d0dfc0fe7847db
Kyle Giammarco <kyle@giammar.co>
2024-08-31 19:15:19 -0400

    initial commit to switch to the new plugin architecture

--- my-theme-plugin/build.gradle.kts
+++ my-theme-plugin/build.gradle.kts
@@ -9,13 +9,13 @@ fleetPlugin {
 
     // STEP 1: chose an ID for your plugin, it must be unique
     //
-    // id = "your.plugin.id.here"
+    id = "shadesmear.theme.dark"
 
     // STEP 2: set up the sensible metadata for your plugin
-    // metadata {
-    //     readableName = "Your plugin name here"
-    //     description = "Your plugin description here"
-    // }
+    metadata {
+        readableName = "ShadeSmear Dark Theme"
+        description = "A dark mellow color scheme for Fleet"
+    }
 
     fleetRuntime {
         version = libs.versions.fleet.runtime
